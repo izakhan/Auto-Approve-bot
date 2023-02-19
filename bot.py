b7b77452b0eebf235442491ddb5a243c8dc751e4
izakhan <121670888+izakhan@users.noreply.github.com>
2023-02-19 15:20:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,6 +38,8 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
+        await app.send_video(chat_id=kk.id, video=img, caption="**Hello {}!\nWelcome To {}\n\n__Powerd By : @Sarbudeen786**".format(m.from_user.mention, m.chat.title)) 
+        add_user(kk.id)
         keyboard = InlineKeyboardMarkup(
                     [
                         InlineKeyboardButton("Movie Updates 🎥", url="https://t.me/+cm1FYByZ5SgzYjc1"),
@@ -46,8 +48,7 @@ async def approve(_, m : Message):
                         InlineKeyboardButton("🎞️ Theatre ReleaseS ✅", url="https://t.me/+nQjZWOoEVpMxOWE1"),
                     ]
                    )
-        await app.send_video(chat_id=kk.id, video=img, caption="**Hello {}!\nWelcome To {}\n\n__Powerd By : @Sarbudeen786**".format(m.from_user.mention, m.chat.title)) 
-        add_user(kk.id)
+        
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
     except Exception as err:
